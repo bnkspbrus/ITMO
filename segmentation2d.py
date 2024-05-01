f67c12589494ff80555c16b0ce04e6d26fa0b763
Your Name <you@example.com>
2024-05-01 17:00:41 +0200

    seg2d

--- segmentation2d.py
+++ segmentation2d.py
@@ -6,6 +6,7 @@ import tqdm
 from PIL import Image
 from transformers import AutoImageProcessor, Mask2FormerForUniversalSegmentation
 from kitti360scripts.helpers.labels import trainId2label
+from torch.utils.data import Dataset, DataLoader
 import matplotlib.pyplot as plt
 
 trainId2labelId = np.zeros(256, dtype=np.uint8)
@@ -32,15 +33,23 @@ model = (Mask2FormerForUniversalSegmentation
          .from_pretrained("facebook/mask2former-swin-large-cityscapes-panoptic")
          .to(device))
 
-for folder in os.listdir(DATA_2D_EQUIRECT):
-    images = os.listdir(os.path.join(DATA_2D_EQUIRECT, folder, 'image_02'))
-    min_frame = min(int(image.split('.')[0]) for image in images)
-    max_frame = max(int(image.split('.')[0]) for image in images)
-    for i in tqdm.tqdm(range(min_frame, max_frame + 1)):
-        image_02 = Image.open(os.path.join(DATA_2D_EQUIRECT, folder, 'image_02', f'{i:010d}.png'))
-        image_03_path = os.path.join(DATA_2D_EQUIRECT, folder, 'image_03', f'{i:010d}.png')
+
+class SidesDataset(Dataset):
+    def __init__(self, folder):
+        self.folder = folder
+        self.images = os.listdir(os.path.join(DATA_2D_EQUIRECT, folder, 'image_02'))
+        self.min_frame = min(int(image.split('.')[0]) for image in self.images)
+        self.max_frame = max(int(image.split('.')[0]) for image in self.images)
+
+    def __len__(self):
+        return self.max_frame - self.min_frame + 1
+
+    def __getitem__(self, idx):
+        i = self.min_frame + idx
+        image_02 = Image.open(os.path.join(DATA_2D_EQUIRECT, self.folder, 'image_02', f'{i:010d}.png'))
+        image_03_path = os.path.join(DATA_2D_EQUIRECT, self.folder, 'image_03', f'{i:010d}.png')
         if not os.path.exists(image_03_path):
-            continue
+            return None
         image_03 = Image.open(image_03_path)
         image_02 = np.roll(np.array(image_02), -350, axis=1)
         image_03 = np.roll(np.array(image_03), 1050, axis=1)
@@ -56,34 +65,56 @@ for folder in os.listdir(DATA_2D_EQUIRECT):
             cube = cubes[ii % 2]
             side = cube[700:1400, 1400 * (j // 3) + 700 * (ii // 2):1400 * (j // 3) + 700 * (ii // 2 + 1)]
             sides.append(side)
-        inputs = processor(images=sides, return_tensors="pt").to(device)
 
-        with torch.no_grad():
-            outputs = model(**inputs)
+        return np.array(sides)
 
-        class_queries_logits = outputs.class_queries_logits
-        masks_queries_logits = outputs.masks_queries_logits
 
-        results = processor.post_process_panoptic_segmentation(outputs, target_sizes=[side.shape[:2] for side in sides])
+def process_result(i, j, result, folder):
+    instances = result["segmentation"]
+    instances = instances.cpu().numpy()
+    instances_path = os.path.join(DATA_2D_SEMANTICS, folder, 'instance', f'{i:010d}', f'{j}.png')
+    os.makedirs(os.path.dirname(instances_path), exist_ok=True)
+    Image.fromarray(instances).save(instances_path)
 
-        for j, result in enumerate(results):
-            instances = result["segmentation"]
-            instances = instances.cpu().numpy()
-            instances_path = os.path.join(DATA_2D_SEMANTICS, folder, 'instance', f'{i:010d}', f'{j}.png')
-            os.makedirs(os.path.dirname(instances_path), exist_ok=True)
-            Image.fromarray(instances).save(instances_path)
+    segments_info = result["segments_info"]
+    id2labelId = np.zeros(len(segments_info) + 2, dtype=np.uint8)
+    for segment_info in segments_info:
+        id2labelId[segment_info["id"]] = segment_info["label_id"]
+    label_ids = id2labelId[instances]
+    semantics = trainId2labelId[label_ids]
+    semantics_path = os.path.join(DATA_2D_SEMANTICS, folder, 'semantic', f'{i:010d}', f'{j}.png')
+    os.makedirs(os.path.dirname(semantics_path), exist_ok=True)
+    Image.fromarray(semantics).save(semantics_path)
 
-            segments_info = result["segments_info"]
-            id2labelId = np.zeros(len(segments_info) + 2, dtype=np.uint8)
-            for segment_info in segments_info:
-                id2labelId[segment_info["id"]] = segment_info["label_id"]
-            label_ids = id2labelId[instances]
-            semantics = trainId2labelId[label_ids]
-            semantics_path = os.path.join(DATA_2D_SEMANTICS, folder, 'semantic', f'{i:010d}', f'{j}.png')
-            os.makedirs(os.path.dirname(semantics_path), exist_ok=True)
-            Image.fromarray(semantics).save(semantics_path)
+    semantics_rgb = trainId2color[label_ids]
+    semantics_rgb_path = os.path.join(DATA_2D_SEMANTICS, folder, 'semantic_rgb', f'{i:010d}', f'{j}.png')
+    os.makedirs(os.path.dirname(semantics_rgb_path), exist_ok=True)
+    Image.fromarray(semantics_rgb).save(semantics_rgb_path)
 
-            semantics_rgb = trainId2color[label_ids]
-            semantics_rgb_path = os.path.join(DATA_2D_SEMANTICS, folder, 'semantic_rgb', f'{i:010d}', f'{j}.png')
-            os.makedirs(os.path.dirname(semantics_rgb_path), exist_ok=True)
-            Image.fromarray(semantics_rgb).save(semantics_rgb_path)
+
+def main():
+    for folder in os.listdir(DATA_2D_EQUIRECT):
+        dataset = SidesDataset(folder)
+        loader = DataLoader(dataset, batch_size=4, num_workers=4, shuffle=False)
+
+        for sides in tqdm.tqdm(loader):
+            if sides is None:
+                continue
+            images = list(sides.reshape(-1, 700, 700, 3))
+            inputs = processor(images=images, return_tensors="pt").to(device)
+
+            with torch.no_grad():
+                outputs = model(**inputs)
+
+            class_queries_logits = outputs.class_queries_logits
+            masks_queries_logits = outputs.masks_queries_logits
+
+            results = processor.post_process_panoptic_segmentation(outputs, target_sizes=list(
+                map(lambda x: x.shape[:2], images)))
+
+            list(map(lambda x: process_result(dataset.min_frame + x[0] // 6, x[0] % 6, x[1], folder),
+                     enumerate(results)))
+
+
+if __name__ == '__main__':
+    main()
