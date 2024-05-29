3462a5aee9d1099728a02efd8cecb21b6b165962
Your Name <you@example.com>
2024-05-30 01:48:49 +0300

    seg 3d

--- semantic.py
+++ semantic.py
@@ -5,6 +5,7 @@ import numpy as np
 import open3d
 
 DATA_3D_SEMANTICS = "data_3d_semantics"
+KITTI_DATA_3D_SEMANTICS = "data_3d_semantics"
 DATA_SEMANTICS = "data_semantics"
 KITTI_360 = 'KITTI_360'
 
@@ -20,7 +21,7 @@ def segment_sequence(folder, file):
     semantics_path = os.path.join(DATA_SEMANTICS, '%04d_%010d_%010d.npy' % (seq, min_frame, max_frame))
     if os.path.exists(semantics_path):
         return
-    ply_file = os.path.join(KITTI_360, DATA_3D_SEMANTICS, 'train', folder, 'static', file)
+    ply_file = os.path.join(KITTI_360, KITTI_DATA_3D_SEMANTICS, 'train', folder, 'static', file)
     points = read_ply(ply_file)
     points = np.array([points['x'], points['y'], points['z']]).T
     semantic3d = np.zeros((points.shape[0], 256), dtype=np.int32)
@@ -39,7 +40,7 @@ def segment_sequence(folder, file):
 
 
 def draw_sequence(folder, file):
-    ply_file = os.path.join(KITTI_360, DATA_3D_SEMANTICS, 'train', folder, 'static', file)
+    ply_file = os.path.join(KITTI_360, KITTI_DATA_3D_SEMANTICS, 'train', folder, 'static', file)
     points = read_ply(ply_file)
     points = np.array([points['x'], points['y'], points['z']]).T
     min_frame, max_frame = os.path.splitext(file)[0].split('_')
@@ -57,7 +58,7 @@ def draw_sequence(folder, file):
 def main():
     os.makedirs(DATA_SEMANTICS, exist_ok=True)
     for folder in os.listdir(DATA_3D_SEMANTICS):
-        statics = os.path.join(KITTI_360, DATA_3D_SEMANTICS, 'train', folder, 'static')
+        statics = os.path.join(KITTI_360, KITTI_DATA_3D_SEMANTICS, 'train', folder, 'static')
         for file in os.listdir(statics):
             segment_sequence(folder, file)
 
--- semantic3d.py
+++ semantic3d.py
@@ -13,6 +13,7 @@ import re
 
 DATA_2D_SEMANTICS = "data_2d_semantics"
 DATA_3D_SEMANTICS = "data_3d_semantics"
+KITTI_DATA_3D_SEMANTICS = "data_3d_semantics"
 DATA_3D_PROJECTION = "data_3d_projection"
 KITTI_360 = 'KITTI_360'
 
@@ -25,7 +26,9 @@ F = 350
 K = np.array([[F, 0, S / 2], [0, F, S / 2], [0, 0, 1]])
 TRN = np.deg2rad(45)
 R1 = np.array([[np.cos(TRN), 0, np.sin(TRN)], [0, 1, 0], [-np.sin(TRN), 0, np.cos(TRN)]]).T
-BALL_RADIUS = 100
+
+
+# BALL_RADIUS = 100
 
 
 class CameraPerspectiveV2(CameraPerspective):
@@ -74,8 +77,8 @@ def project_vertices_halfball(cam, vertices, frameId, side0, folder):
         u, v, depth = cam_00.cam2image(points_local)
         mask = (u >= 0) & (u < S) & (v >= 0) & (v < S) & (depth > 0)
         u, v, depth = u[mask], v[mask], depth[mask]
-        u, v, dmask = z_buffer(u, v, depth)
-        mask[mask] = dmask
+        # u, v, dmask = z_buffer(u, v, depth)
+        # mask[mask] = dmask
         os.makedirs(os.path.join(DATA_3D_PROJECTION, folder, f'{frameId:010d}'), exist_ok=True)
         np.save(os.path.join(DATA_3D_PROJECTION, folder, f'{frameId:010d}', f'u_{side}.npy'), u)
         np.save(os.path.join(DATA_3D_PROJECTION, folder, f'{frameId:010d}', f'v_{side}.npy'), v)
@@ -121,7 +124,7 @@ def segment_semantic_ball(frame, points, colors, folder, ball):
 def draw_semantic(frame, folder, file):
     semantic3d = np.load(os.path.join(DATA_3D_SEMANTICS, folder, 'semantic', f'{frame:010d}.npy'))
     ball = np.load(os.path.join(DATA_3D_SEMANTICS, folder, 'ball', f'{frame:010d}.npy'))
-    statics = os.path.join(KITTI_360, DATA_3D_SEMANTICS, 'train', folder, 'static')
+    statics = os.path.join(KITTI_360, KITTI_DATA_3D_SEMANTICS, 'train', folder, 'static')
     ply = read_ply(os.path.join(statics, file))
     points = np.array([ply['x'], ply['y'], ply['z']]).T
     points = points[ball]
@@ -132,17 +135,20 @@ def draw_semantic(frame, folder, file):
     open3d.visualization.draw_geometries_with_editing([pcd])
 
 
-def search_ball(pose, kdtree, folder):
-    frame = int(pose[0])
-    curr_pose = pose[1:]
-    curr_pose = np.reshape(curr_pose, [3, 4])
-    T = curr_pose[:3, 3]
+def search_ball(curr_pose, points, folder, frame, radius):
     ball_path = os.path.join(DATA_3D_SEMANTICS, folder, 'ball', f'{frame:010d}.npy')
     if os.path.exists(ball_path):
-        return
-    ball = kdtree.search_radius_vector_3d(T, BALL_RADIUS)
+        return np.load(ball_path)
+    # curr_pose = np.reshape(curr_pose, [3, 4])
+    T = curr_pose[:3, 3]
+    pcd = open3d.geometry.PointCloud()
+    pcd.points = open3d.utility.Vector3dVector(points)
+    # search ball with hidden_point_removal
+    ball = pcd.hidden_point_removal(T, radius)[1]
+    # ball = kdtree.search_radius_vector_3d(T, BALL_RADIUS)
     os.makedirs(os.path.dirname(ball_path), exist_ok=True)
-    np.save(ball_path, ball[1])
+    np.save(ball_path, ball)
+    return ball
 
 
 class FrameDataset(Dataset):
@@ -155,20 +161,26 @@ class FrameDataset(Dataset):
         valid_frames = valid_frames[(valid_frames >= min_frame) & (valid_frames <= max_frame)]
         valid_mask = np.isin(frames, valid_frames)
         self.frames = frames[valid_mask]
-        poses = poses[valid_mask].reshape(-1, 3 * 4)
-        fpath = os.path.join(KITTI_360, DATA_3D_SEMANTICS, 'train', folder, 'static', fname)
+        self.poses = poses[valid_mask].reshape(-1, 3 * 4)
+        fpath = os.path.join(KITTI_360, KITTI_DATA_3D_SEMANTICS, 'train', folder, 'static', fname)
         ply = read_ply(fpath)
         self.points = np.array([ply['x'], ply['y'], ply['z']]).T
         self.colors = np.array([ply['red'], ply['green'], ply['blue']]).T
-        kdtree = get_kdtree(self.points)
-        np.apply_along_axis(search_ball, 1, np.column_stack((self.frames, poses)), kdtree, folder)
+        pcd = open3d.geometry.PointCloud()
+        pcd.points = open3d.utility.Vector3dVector(self.points)
+        diameter = np.linalg.norm(
+            np.asarray(pcd.get_max_bound()) - np.asarray(pcd.get_min_bound()))
+        self.radius = diameter * 1000
+        cam_02 = CameraFisheye(KITTI_360, seq=folder, cam_id=2)
+        self.cam2world = cam_02.cam2world
 
     def __len__(self):
         return len(self.frames)
 
     def __getitem__(self, idx):
         frame = self.frames[idx]
-        ball = np.load(os.path.join(DATA_3D_SEMANTICS, self.folder, 'ball', f'{frame:010d}.npy'))
+        curr_pose = self.cam2world[frame]
+        ball = search_ball(curr_pose, self.points, self.folder, frame, self.radius)
         project_vertices_ball(frame, self.points, self.folder, self.cam_02, self.cam_03, ball)
         segment_semantic_ball(frame, self.points, self.colors, self.folder, ball)
         return 0
@@ -176,13 +188,13 @@ class FrameDataset(Dataset):
 
 def main():
     for folder in os.listdir(DATA_2D_SEMANTICS):
-        statics = os.path.join(KITTI_360, DATA_3D_SEMANTICS, 'train', folder, 'static')
+        statics = os.path.join(KITTI_360, KITTI_DATA_3D_SEMANTICS, 'train', folder, 'static')
         frames, poses = frames_poses(folder)
         cam_02 = CameraFisheye(KITTI_360, folder, 2)
         cam_03 = CameraFisheye(KITTI_360, folder, 3)
         for file in os.listdir(statics):
             dataset = FrameDataset(folder, file, frames, poses, cam_02, cam_03)
-            dataloader = DataLoader(dataset, batch_size=1, num_workers=1, shuffle=False)
+            dataloader = DataLoader(dataset, batch_size=4, num_workers=4, shuffle=False)
             list(tqdm(dataloader, total=len(dataloader)))
 
 
