55c8045e89dc3d48300e3987d39f11e37c338cb7
Nikita Barsukov <80609501+bnkspbrus@users.noreply.github.com>
2024-06-02 11:59:00 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,172 +1,92 @@
-#!/bin/bash
+FROM nvcr.io/nvidia/cuda:10.2-devel-ubuntu18.04
+ENV DEBIAN_FRONTEND noninteractive
+RUN useradd -ms /bin/bash --uid 1000 jupyter\
+&& apt update\
+&& apt install -y python3.8-dev python3.8-distutils curl\
+&& ln -s /usr/bin/python3.8 /usr/local/bin/python3\
+&& curl https://bootstrap.pypa.io/get-pip.py | python3
 
-# Recover the project directory from the position of the install.sh script
-HERE=`dirname $0`
-HERE=`realpath $HERE`
+RUN apt-get update \
+&& apt-get install sudo \
+&& usermod -aG sudo jupyter
 
+ENV LANG=C.UTF-8 LC_ALL=C.UTF-8
+ENV PATH /opt/conda/bin:$PATH
 
-# Local variables
-PROJECT_NAME=deep_view_aggregation
-YML_FILE=${HERE}/${PROJECT_NAME}.yml
-TORCH=1.7.1
-CUDA_SUPPORTED=(10.1 10.2 11.0 11.2 11.4)
+# renovate: datasource=custom.anaconda_installer
+ARG INSTALLER_URL_LINUX64="https://repo.anaconda.com/archive/Anaconda3-2022.05-Linux-x86_64.sh"
+ARG SHA256SUM_LINUX64="a7c0afe862f6ea19a596801fc138bde0463abcbce1b753e8d5c474b506a2db2d"
+# renovate: datasource=custom.anaconda_installer
+ARG INSTALLER_URL_S390X="https://repo.anaconda.com/archive/Anaconda3-2022.05-Linux-s390x.sh"
+ARG SHA256SUM_S390X="c14415df69e439acd7458737a84a45c6067376cbec2fccf5e2393f9837760ea7"
+# renovate: datasource=custom.anaconda_installer
+ARG INSTALLER_URL_AARCH64="https://repo.anaconda.com/archive/Anaconda3-2022.05-Linux-aarch64.sh"
+ARG SHA256SUM_AARCH64="dc6bb4eab3996e0658f8bc4bbd229c18f55269badd74acc36d9e23143268b795"
 
+# hadolint ignore=DL3008
+RUN set -x && \
+    apt-get update --fix-missing && \
+    apt-get install -y --no-install-recommends \
+        bzip2 \
+        ca-certificates \
+        git \
+        libglib2.0-0 \
+        libsm6 \
+        libxcomposite1 \
+        libxcursor1 \
+        libxdamage1 \
+        libxext6 \
+        libxfixes3 \
+        libxi6 \
+        libxinerama1 \
+        libxrandr2 \
+        libxrender1 \
+        mercurial \
+        openssh-client \
+        procps \
+        subversion \
+        wget \
+    && apt-get clean \
+    && rm -rf /var/lib/apt/lists/* && \
+    UNAME_M="$(uname -m)" && \
+    if [ "${UNAME_M}" = "x86_64" ]; then \
+        INSTALLER_URL=${INSTALLER_URL_LINUX64}; \
+        SHA256SUM=${SHA256SUM_LINUX64}; \
+    elif [ "${UNAME_M}" = "s390x" ]; then \
+        INSTALLER_URL=${INSTALLER_URL_S390X}; \
+        SHA256SUM=${SHA256SUM_S390X}; \
+    elif [ "${UNAME_M}" = "aarch64" ]; then \
+        INSTALLER_URL=${INSTALLER_URL_AARCH64}; \
+        SHA256SUM=${SHA256SUM_AARCH64}; \
+    fi && \
+    wget "${INSTALLER_URL}" -O anaconda.sh -q && \
+    echo "${SHA256SUM} anaconda.sh" > shasum && \
+    sha256sum --check --status shasum && \
+    /bin/bash anaconda.sh -b -p /opt/conda && \
+    rm anaconda.sh shasum && \
+    ln -s /opt/conda/etc/profile.d/conda.sh /etc/profile.d/conda.sh && \
+    echo ". /opt/conda/etc/profile.d/conda.sh" >> ~/.bashrc && \
+    echo "conda activate base" >> ~/.bashrc && \
+    find /opt/conda/ -follow -type f -name '*.a' -delete && \
+    find /opt/conda/ -follow -type f -name '*.js.map' -delete && \
+    /opt/conda/bin/conda clean -afy
 
-# Installation script for Anaconda3 environments
-echo "#############################################"
-echo "#                                           #" 
-echo "#           Deep View Aggregation           #"
-echo "#                 Installer                 #"
-echo "#                                           #" 
-echo "#############################################"
-echo
-echo
+ARG ENV_YAML_URL="https://raw.githubusercontent.com/bnkspbrus/ITMO/main/deep_view_aggregation.yml"
+RUN wget "${ENV_YAML_URL}" -O deep_view_aggregation.yml
 
+ENV CUDA_HOME /usr/local/cuda-10.2
+ARG INSTALL_SH="https://raw.githubusercontent.com/bnkspbrus/ITMO/main/install.sh"
+RUN wget "${INSTALL_SH}" -O install.sh -q \
+&& /bin/bash install.sh
 
-echo "_______________ Prerequisites _______________"
-echo "  - conda"
-echo "  - cuda >= 10.1 (tested with `echo ${CUDA_SUPPORTED[*]}`)"
-echo "  - gcc >= 7"
-echo
-echo
-
-
-echo "____________ Pick conda install _____________"
-echo
-# Recover the path to conda on your machine
-CONDA_DIR=/opt/conda
-
-while (test -z $CONDA_DIR) || [ ! -d $CONDA_DIR ]
-do
-    echo "Could not find conda at: "$CONDA_DIR
-    read -p "Please provide you conda install directory: " CONDA_DIR
-    CONDA_DIR=`realpath $CONDA_DIR`
-done
-
-echo "Using conda conda found at: ${CONDA_DIR}/etc/profile.d/conda.sh"
-source ${CONDA_DIR}/etc/profile.d/conda.sh
-echo
-echo
-
-
-echo "_____________ Pick CUDA version _____________"
-echo
-
-CUDA_SUPPORTED=(10.2 11.2 11.4)
-CUDA_VERSION=`nvcc --version | grep release | sed 's/.* release //' | sed 's/, .*//'`
-CUDA_MAJOR=`echo $CUDA_VERSION | sed 's/\..*//'`
-CUDA_MINOR=`echo $CUDA_VERSION | sed 's/.*\.//'`
-CUDA_MINOR_TORCH=`echo $CUDA_MINOR`
-CUDA_MINOR_TV=`echo $CUDA_MINOR`
-CUDA_MINOR_PYG=`echo $CUDA_MINOR`
-
-# If CUDA version not supported, ask whether to proceed
-if [[ ! " ${CUDA_SUPPORTED[*]} " =~ " ${CUDA_VERSION} " ]]
-then
-    echo "Found CUDA ${CUDA_VERSION} installed, is not among tested versions: "`echo ${CUDA_SUPPORTED[*]}`
-    echo "This may cause downstream errors when installing PyTorch and PyTorch Geometric dependencies, which you might solve by manually modifying setting the wheels in this script."
-    read -p "Proceed anyways ? [y/n] " -n 1 -r; echo
-    if !(test -z $REPLY) && [[ ! $REPLY =~ ^[Yy]$ ]]
-    then
-        exit 1
-    fi
-fi
-
-# Adjusting CUDA minor to match existing PyTorch and PyTorch Geometric
-# wheels
-if [[ ${CUDA_MAJOR} == 10 && ${CUDA_MINOR} == 0 ]]
-then
-    CUDA_MINOR_TORCH=1
-    CUDA_MINOR_TV=1
-    CUDA_MINOR_PYG=1
-fi
-if [[ ${CUDA_MAJOR} == 10 && ${CUDA_MINOR} > 1 ]]
-then
-    CUDA_MINOR_TORCH=2
-    CUDA_MINOR_TV=1
-    CUDA_MINOR_PYG=2
-fi
-if [[ ${CUDA_MAJOR} == 11 ]]
-then
-    CUDA_MINOR_TORCH=0
-    CUDA_MINOR_TV=0
-    CUDA_MINOR_PYG=0
-fi
-
-# Summing up used CUDA versions
-cuXXX_TORCH=`echo "cu"${CUDA_MAJOR}${CUDA_MINOR_TORCH}`
-cuXXX_TV=`echo "cu"${CUDA_MAJOR}${CUDA_MINOR_TV}`
-cuXXX_PYG=`echo "cu"${CUDA_MAJOR}${CUDA_MINOR_PYG}`
-echo "CUDA version installed: ${CUDA_MAJOR}.${CUDA_MINOR}"
-echo "CUDA version used for Torch: ${CUDA_MAJOR}.${CUDA_MINOR_TORCH}"
-echo "CUDA version used for TorchVision: ${CUDA_MAJOR}.${CUDA_MINOR_TV}"
-echo "CUDA version used for Torch Geometric: ${CUDA_MAJOR}.${CUDA_MINOR_PYG}"
-echo
-echo
-
-
-echo "________________ Installation _______________"
-echo
-
-# Create deep_view_aggregation environment from yml
-conda env create -f ${YML_FILE}
-
-# Activate the env
-source ${CONDA_DIR}/etc/profile.d/conda.sh  
-conda activate ${PROJECT_NAME}
-
-# Dependencies not installed from the .yml
-# See https://pytorch.org/get-started/previous-versions/ if wheel issues
-pip install torch==${TORCH} torchvision==0.8.2 --no-cache-dir -f https://download.pytorch.org/whl/cu102/torch_stable.html
-pip install torchnet
-
-# Install torch-geometric and dependencies
-# See https://pytorch-geometric.com/whl/ if wheel issues
-pip install --no-index torch-scatter -f https://pytorch-geometric.com/whl/torch-${TORCH}+${cuXXX_PYG}.html
-pip install --no-index torch-sparse -f https://pytorch-geometric.com/whl/torch-${TORCH}+${cuXXX_PYG}.html
-pip install --no-index torch-cluster -f https://pytorch-geometric.com/whl/torch-${TORCH}+${cuXXX_PYG}.html
-pip install --no-index torch-spline-conv -f https://pytorch-geometric.com/whl/torch-${TORCH}+${cuXXX_PYG}.html
-pip install torch-geometric==1.6.3
-FORCE_CUDA=1 PATH=$CUDA_HOME/bin:$PATH CPATH=$CUDA_HOME/include:$CPATH pip install torch-points-kernels==0.6.10 --no-cache-dir
-
-# Additional dependencies
-pip install omegaconf
-pip install wandb
-pip install tensorboard
-pip install plyfile
-pip install hydra-core==1.1.0
-pip install pytorch-metric-learning
-pip install matplotlib
-pip install seaborn
-pip install pykeops==1.4.2
-pip install imageio
-pip install opencv-python
-pip install pypng
-pip install git+http://github.com/CSAILVision/semantic-segmentation-pytorch.git@master
-pip install h5py
-pip install faiss-gpu===1.6.5
-
-# Install MinkowskiEngine
-apt-get update && apt-get clean && apt-get install -y libopenblas-dev
-pip install -U MinkowskiEngine==v0.4.3 -v --no-deps --install-option="--blas=openblas" --install-option="--force_cuda"
-
-# Install torchsparse
-apt-get update && apt-get clean && apt-get install -y libsparsehash-dev
-pip install --upgrade git+https://github.com/mit-han-lab/torchsparse.git@v1.1.0
-#pip install --upgrade git+https://github.com/mit-han-lab/torchsparse.git@v1.4.0
-
-# Install plotly and associated jupyter requirements
-pip install plotly==5.4.0
-pip install "jupyterlab>=3" "ipywidgets>=7.6"
-pip install jupyter-dash
-#jupyter labextension install @jupyter-widgets/jupyterlab-manager plotlywidget@4.14.1
-npm i @jupyter-widgets/jupyterlab-manager
-npm i plotlywidget@4.14.1
-echo "NB: In case you want to use our notebooks, make sure the 'deep_view_aggregation' kernel is accessible in jupyterlab"
-echo
-echo
-
-echo "Successfully installed Deep View Aggregation."
-
-echo
-echo
+RUN conda env list
+ENV PATH="/opt/conda/envs/deep_view_aggregation/bin:$PATH"
+ENV CONDA_PREFIX="/opt/conda/envs/deep_view_aggregation"
+ENV CONDA_SHLVL="2"
+ENV CONDA_DEFAULT_ENV="deep_view_aggregation"
+ENV CONDA_PROMPT_MODIFIER="(deep_view_aggregation) "
+ENV CONDA_EXE="/opt/conda/bin/conda"
+ENV _CE_M=""
+ENV _CE_CONDA=""
+ENV CONDA_PYTHON_EXE="/opt/conda/bin/python"
+ENV CONDA_PREFIX_1="/opt/conda"
