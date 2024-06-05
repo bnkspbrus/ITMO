e4b9039dd595575a1c443da331d9752d4c9fdaba
Nikita Barsukov <80609501+bnkspbrus@users.noreply.github.com>
2024-06-05 18:28:05 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -117,6 +117,7 @@ conda activate ${PROJECT_NAME}
 
 # Dependencies not installed from the .yml
 # See https://pytorch.org/get-started/previous-versions/ if wheel issues
+pip install typing_extensions==4.8.0
 pip install torch==${TORCH}+${cuXXX_TORCH} torchvision==0.8.2+${cuXXX_TV} --no-cache-dir -f https://download.pytorch.org/whl/torch_stable.html
 pip install torchnet
 
