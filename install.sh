498739c738813195ad451b8292d87e2ba13fb19f
Nikita Barsukov <80609501+bnkspbrus@users.noreply.github.com>
2024-06-05 03:54:22 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -117,7 +117,7 @@ conda activate ${PROJECT_NAME}
 
 # Dependencies not installed from the .yml
 # See https://pytorch.org/get-started/previous-versions/ if wheel issues
-conda install pytorch==1.7.1 torchvision==0.8.2 cudatoolkit=10.2 -c pytorch
+conda install pytorch==1.7.1 torchvision==0.8.2 cudatoolkit=10.2 -c pytorch -c nvidia
 pip install torchnet
 
 # Install torch-geometric and dependencies
