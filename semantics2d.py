b2a6ae9d8d005a52664de72b60ce58a475ae8f46
Your Name <you@example.com>
2024-06-09 02:08:08 +0300

    semantics2d.py

--- semantics2d.py
+++ semantics2d.py
@@ -36,18 +36,25 @@ model = (Mask2FormerForUniversalSegmentation
 
 
 class SidesDataset(Dataset):
-    def __init__(self, folder):
-        self.folder = folder
-        self.files = os.listdir(osp.join(DATA_2D_EQUIRECT, folder, 'image_02'))
-        self.files = list(filter(lambda x: x.endswith('.png'), self.files))
+    def __init__(self, sequence, image_lower_bound, image_upper_bound):
+        self.sequence = sequence
+        self.frames = os.listdir(osp.join(DATA_2D_EQUIRECT, sequence, 'image_02'))
+        self.frames = list(filter(lambda x: x.endswith('.png'), self.frames))
+        self.frames = list(
+            filter(lambda x: image_lower_bound <= int(x.split('.')[0]) <= image_upper_bound, self.frames))
+        # filter already processed frames
+        self.frames = list(
+            filter(lambda x: not osp.exists(osp.join(DATA_2D_SEMANTICS, sequence, 'semantic', x.split("."), '0.png')),
+                   self.frames))
 
     def __len__(self):
-        return len(self.files)
+        return len(self.frames)
 
     def __getitem__(self, idx):
-        file = self.files[idx]
-        image_02 = Image.open(osp.join(DATA_2D_EQUIRECT, self.folder, 'image_02', file))
-        image_03_path = osp.join(DATA_2D_EQUIRECT, self.folder, 'image_03', file)
+        frame = self.frames[idx]
+        image_02_path = osp.join(DATA_2D_EQUIRECT, self.sequence, 'image_02', frame)
+        image_02 = Image.open(image_02_path)
+        image_03_path = osp.join(DATA_2D_EQUIRECT, self.sequence, 'image_03', frame)
         if not osp.exists(image_03_path):
             return None
         image_03 = Image.open(image_03_path)
@@ -66,13 +73,13 @@ class SidesDataset(Dataset):
             side = cube[700:1400, 1400 * (j // 3) + 700 * (ii // 2):1400 * (j // 3) + 700 * (ii // 2 + 1)]
             sides.append(side)
 
-        return np.array(sides), int(file.split('.')[0])
+        return np.array(sides), int(frame.split('.')[0])
 
 
-def process_result(i, j, result, folder):
+def process_result(i, j, result, sequence):
     instances = result["segmentation"]
     instances = instances.cpu().numpy()
-    instances_path = osp.join(DATA_2D_SEMANTICS, folder, 'instance', f'{i:010d}', f'{j}.png')
+    instances_path = osp.join(DATA_2D_SEMANTICS, sequence, 'instance', f'{i:010d}', f'{j}.png')
     os.makedirs(osp.dirname(instances_path), exist_ok=True)
     Image.fromarray(instances).save(instances_path)
 
@@ -82,41 +89,40 @@ def process_result(i, j, result, folder):
         id2labelId[segment_info["id"]] = segment_info["label_id"]
     label_ids = id2labelId[instances]
     semantics = trainId2labelId[label_ids]
-    semantics_path = osp.join(DATA_2D_SEMANTICS, folder, 'semantic', f'{i:010d}', f'{j}.png')
+    semantics_path = osp.join(DATA_2D_SEMANTICS, sequence, 'semantic', f'{i:010d}', f'{j}.png')
     os.makedirs(osp.dirname(semantics_path), exist_ok=True)
     Image.fromarray(semantics).save(semantics_path)
 
     semantics_rgb = trainId2color[label_ids]
-    semantics_rgb_path = osp.join(DATA_2D_SEMANTICS, folder, 'semantic_rgb', f'{i:010d}', f'{j}.png')
+    semantics_rgb_path = osp.join(DATA_2D_SEMANTICS, sequence, 'semantic_rgb', f'{i:010d}', f'{j}.png')
     os.makedirs(osp.dirname(semantics_rgb_path), exist_ok=True)
     Image.fromarray(semantics_rgb).save(semantics_rgb_path)
 
 
-def main():
-    for folder in os.listdir(DATA_2D_EQUIRECT):
-        dataset = SidesDataset(folder)
-        loader = DataLoader(dataset, batch_size=4, num_workers=4, shuffle=False)
+def process_sequence(sequence, image_lower_bound, image_upper_bound):
+    dataset = SidesDataset(sequence, image_lower_bound, image_upper_bound)
+    loader = DataLoader(dataset, batch_size=4, num_workers=4, shuffle=False)
 
-        for sides, frame in tqdm(loader):
-            if sides is None:
-                continue
-            images = list(sides.reshape(-1, 700, 700, 3))
-            inputs = processor(images=images, return_tensors="pt").to(device)
+    for sides, frame in tqdm(loader):
+        if sides is None:
+            continue
+        images = list(sides.reshape(-1, 700, 700, 3))
+        inputs = processor(images=images, return_tensors="pt").to(device)
 
-            with torch.no_grad():
-                outputs = model(**inputs)
+        with torch.no_grad():
+            outputs = model(**inputs)
 
-            class_queries_logits = outputs.class_queries_logits
-            masks_queries_logits = outputs.masks_queries_logits
+        class_queries_logits = outputs.class_queries_logits
+        masks_queries_logits = outputs.masks_queries_logits
 
-            results = processor.post_process_panoptic_segmentation(outputs, target_sizes=list(
-                map(lambda x: x.shape[:2], images)))
+        results = processor.post_process_panoptic_segmentation(outputs, target_sizes=list(
+            map(lambda x: x.shape[:2], images)))
 
-            frame = frame.repeat_interleave(6)
+        frame = frame.repeat_interleave(6)
 
-            list(map(lambda x: process_result(x[0], x[1][0] % 6, x[1][1], folder),
-                     zip(frame, enumerate(results))))
+        list(map(lambda x: process_result(x[0], x[1][0] % 6, x[1][1], sequence),
+                 zip(frame, enumerate(results))))
 
 
 if __name__ == '__main__':
-    main()
+    process_sequence('2013_05_28_drive_0000_sync', 2, 385)
