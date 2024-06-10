3121ed46009cf3283d769a96e92e9fbccd86a9bc
Your Name <you@example.com>
2024-06-10 14:59:30 +0300

    seg 3d

--- unimodal_hdmapnet.py
+++ unimodal_hdmapnet.py
@@ -2,23 +2,24 @@ from kitti360_config import WINDOWS, SEQUENCES
 from fisheye2equirect import process_sequence as process_fisheye_sequence
 from panoptic2d import process_sequence as process_panoptic_sequence
 from semantic3d import process_sequence as process_3d_semantic_sequence
-from semantic_hdmap import process_sequence as process_hdmap_sequence
+from semantic_hdmap import process_sequence as process_hdmap_semantic_sequence
+from semantic2d import process_sequence as process_2d_semantic_sequence
 
 
-def eval_semantic(model_name='mask2former', merge_method='cluster'):
+def eval_semantic(model_name='segformer', merge_method='cluster'):
     for window in WINDOWS['val']:
         sequence = window.split('/')[0]
-        image_lower_bound, image_upper_bound = map(int, window.split('/')[1].split('_'))
-        out_02, out_03 = process_fisheye_sequence(sequence, image_lower_bound, image_upper_bound)
+        min_frame, max_frame = map(int, window.split('/')[1].split('_'))
+        rect_02, rect_03, frames = process_fisheye_sequence(sequence, min_frame, max_frame)
         if model_name == 'segformer':
-            # throw not implemented error
-            raise NotImplementedError
+            semantics, frames = process_2d_semantic_sequence(sequence, min_frame, max_frame, model_name=model_name,
+                                                             rect_02=rect_02, rect_03=rect_03, frames=frames)
         elif model_name == 'mask2former' or model_name == 'oneformer':
-            process_panoptic_sequence(sequence, image_lower_bound, image_upper_bound, model_name=model_name)
+            process_panoptic_sequence(sequence, min_frame, max_frame, model_name=model_name)
         else:
             raise ValueError('Invalid model name')
-        process_3d_semantic_sequence(sequence, image_lower_bound, image_upper_bound)
-        process_hdmap_sequence(sequence, image_lower_bound, image_upper_bound)
+        process_3d_semantic_sequence(sequence, min_frame, max_frame)
+        process_hdmap_semantic_sequence(sequence, min_frame, max_frame)
 
 
 def main():
